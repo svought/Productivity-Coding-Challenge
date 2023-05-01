8f4aff898c7b65e2c65df25022462bd81bf797b9
svought <superstv08@gmail.com>
2023-05-01 15:49:24 -0600

    Added Gson to dependencies.

--- src/RepoContributions.java
+++ src/RepoContributions.java
@@ -1,4 +1,9 @@
+import java.io.IOException;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class RepoContributions {
 
